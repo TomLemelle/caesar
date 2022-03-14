8b36bd8b93146ec49805eaa950cb840c4ea54527
Tom Lemelle <tom@web-citizens.com>
2022-03-14 13:50:07 +0100

    :sparkles: Fibonnacci function is working

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 const { circleArea } = require( './circleArea');
 const { isSunday } = require('./isSunday');
+const { listFibonacci } = require('./Fibonnacci');
 
 console.log(circleArea(10));
 console.log(isSunday());
+console.log(listFibonacci(10))
