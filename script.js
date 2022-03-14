d553ed1ffee557f7373df44d3d407015c853f20d
Tom Lemelle <tom@web-citizens.com>
2022-03-14 14:56:54 +0100

    :sparkles: brute force ceasar function is working

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 const { circleArea } = require( './circleArea');
 const { isSunday } = require('./isSunday');
 const { listFibonacci } = require('./Fibonnacci');
+const { bruteForce } = require('./cesar');
 
 console.log(circleArea(10));
 console.log(isSunday());
-console.log(listFibonacci(10))
+console.log(listFibonacci(10));
+console.log(bruteForce());
